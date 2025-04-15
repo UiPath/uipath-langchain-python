d12723ef1fa3723400acc26bd7aa56aef15653f7
Ion Mincu <ion.mincu@uipath.com>
2025-04-15 13:06:16 +0300

    Update middlewares.py

--- src/uipath_langchain/middlewares.py
+++ src/uipath_langchain/middlewares.py
@@ -3,7 +3,7 @@ from uipath._cli.middlewares import Middlewares
 from ._cli.cli_init import langgraph_init_middleware
 from ._cli.cli_run import langgraph_run_middleware
 
-
+# test pr
 def register_middleware():
     """This function will be called by the entry point system when uipath_langchain is installed"""
     Middlewares.register("init", langgraph_init_middleware)
